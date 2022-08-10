46cb69dc0f11156f8885c08bb96505da9153c908
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-08-11 00:55:24 +0530

    Added on click event

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ for(let i=1; i<=16;i++)
     {     
         const box = document.createElement('div');
         box.textContent = ""+i;
-        box.style.cssText = ' border: 2px solid;';
+        box.style.cssText = ' border: 2px solid;'; 
+        box.style.borderColor = "red";
         row.append(box);
+        box.addEventListener('click', (event) => {
+            box.style.cssText = ' background-color: black;';   
+          });
     }
 }
+
+/*box.addEventListener('click', alertFunction);
+*/
