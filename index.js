a3a3b7b9f32e67d57b6ab4f78623faea0ec1b64a
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-08-11 15:36:00 +0530

    added seperate class in css file

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-let s;
-const container = document.querySelector('#size'); 
+let s, c=1;
+document.querySelector('#size'); 
 size.addEventListener('click', (event) => {
-    s = prompt("enter size");
-    if(s <= 64)
-        generate();
-    else    
-        alert("Oops, try something lower.");
+    if(c==1){
+        s = prompt("enter size");
+        if(s <= 64) {
+            generate();
+            c++;
+        }
+        else    
+            alert("Oops, try something lower.");
+    }
+    else {
+        s = prompt("enter size");
+        destroy();
+        c++;
+    }
 });
 
 function generate()
 {
     for(let i=1; i<=s;i++)
     {
-        const container = document.querySelector('#container'); 
+        const container = document.querySelector('#container');
         const row= document.createElement('div');
         container.append(row);
         for(let j=1;j<=s;j++)
         {     
-        const box = document.createElement('div');
-        box.textContent = ""+i;
-        row.append(box);
-        box.style.cssText = ' border: 2px solid;'; 
-        box.style.borderColor = "red";
-        /* 
-        box.addEventListener('click', (event) => {
-            box.style.cssText = ' background-color: black;';   
-        }); */
-        box.addEventListener('mouseenter', (event) => {
-        box.style.cssText = ' background-color: green;  border: 2px solid;';   
-        });
+            const box = document.createElement('div');
+            box.textContent = ""+i;
+            row.append(box);
+            box.style.cssText = ' border: 2px solid;'; 
+            box.style.borderColor = "red";
+            box.addEventListener('mouseenter', (event) => {
+            box.classList.add("cool");
+            });
         }
-    }
+    }       
 }
 
-/*box.addEventListener('click', alertFunction);
+function destroy(){
+    box.classList.remove("cool");
+}
+/*
+box.style.cssText = ' background-color: green;  border: 2px solid;';   
+
+
+document.querySelector('#clear'); 
+clear.addEventListener('click', (event) => { 
+    erase();
+});
+
+
+function erase()
+{
+    const nodelist = document.querySelectorAll('box');
+    nodelist.forEach((box) => {
+    box.classList.remove("cool");
+    });
+}   
 */
