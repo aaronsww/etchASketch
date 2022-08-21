7dae7a932ad0cc404efe42e2fe8cd63b99185683
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2022-08-21 12:28:20 +0530

    Split generate() for user input

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+function firstGrid()
+{
+    for(let i=1; i<=25;i++)
+    {
+        const container = document.querySelector('#container');
+        const row= document.createElement('div');
+        container.append(row);
+        for(let j=1;j<=25;j++)
+        {     
+            const box = document.createElement('div');
+            box.textContent = ""+i;
+            row.appendChild(box);
+            box.style.cssText = ' border: 2px solid;'; 
+            box.style.borderColor = "red";
+            box.addEventListener('mouseenter', (event) => {
+            box.classList.add("fill");
+            });
+        }
+    }       
+}
+
+firstGrid();
+
 let s;
 document.querySelector('#size'); 
 size.addEventListener('click', (event) => {
         s = prompt("enter size");
         if(s <= 64) {
+            const container = document.querySelector('#container');
+            container.removeChild(row); 
             generate();
         }
         else    
@@ -15,7 +40,7 @@ function generate()
     {
         const container = document.querySelector('#container');
         const row= document.createElement('div');
-        container.append(row);
+        container.appendChild(row);
         for(let j=1;j<=s;j++)
         {     
             const box = document.createElement('div');
@@ -29,7 +54,6 @@ function generate()
         }
     }       
 }
-
 
 
 /*   
